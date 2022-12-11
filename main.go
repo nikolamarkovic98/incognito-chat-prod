a4e1f8a0c7522ce55648c4bb1d5e8da36c5f3671
Nikola <nikola9988markovic@gmail.com>
2022-12-11 19:19:34 +0100

    prod

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	return ":" + port
+}
+
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	var filename string
+
+	if strings.Contains(r.URL.Path, ".js") {
+		filename = fmt.Sprintf("./static%s", r.URL.Path)
+		w.Header().Set("Content-Type", "text/javascript")
+	} else if strings.Contains(r.URL.Path, ".css") {
+		filename = fmt.Sprintf("./static%s", r.URL.Path)
+		w.Header().Set("Content-Type", "text/css")
+	} else {
+		filename = fmt.Sprintf("./static/index.html")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	}
+
+	http.ServeFile(w, r, filename)
+}
+
+func main() {
+	port := getPort()
+	router := mux.NewRouter()
+
+	router.PathPrefix("/").HandlerFunc(serve)
+
+	fmt.Println("Server listening on port 3000")
+	http.ListenAndServe(port, router)
+}
